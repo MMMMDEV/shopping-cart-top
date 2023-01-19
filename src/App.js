1418a25bfbf25aa5b45f3499c3de8560de9ec2e7
Mario Duran <manriqueduran123@gmail.com>
2023-01-18 21:43:04 -0600

    bug

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
-import { Route, Routes } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/Shop" element={<Shop />} />
-        <Route path="/Shop/Cart" element={<Cart />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/Shop" element={<Shop />} />
+        <Route exact path="/Shop/Cart" element={<Cart />} />
+      </Switch>
     </div>
   );
 }
